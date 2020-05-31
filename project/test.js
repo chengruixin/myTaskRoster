6a962efc9d6f96cc2287e8beec50a133ace0b747
a1738711 <a1738711@student.adelaide.edu.au>
2020-06-01 03:05:52 +0800

    connect to db

--- project/test.js
+++ project/test.js
@@ -1,9 +1,14 @@
 const mysql = require('mysql');
 const con = mysql.createConnection({
-    host : 'localhost'
+    host : 'localhost',
+
+    //optional for local dev
+    user : 'root',
+    password : 'password',
+
 });
 
 con.connect( err => {
     if(err) console.log(err);
     else console.log("connected");
-})
+});
